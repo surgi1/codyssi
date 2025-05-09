5cca472d89fcaf57f060dbc57d4702f7f74265c0
Jan Surovec <surgi1@gmail.com>
2025-05-09 21:45:15 +0200

    offline for day17

---
+++ 2025/17/script.js
@@ -0,0 +1,21 @@
+/*
+idea:
+steps: 1, 3
+
+0: 1 (jsem tam)
+1: 1
+2: o 1 z 1 = 1
+3: o 1 z 2 = 1 + o 3 z 0 = 1 => 2
+4: o 1 z 3 = 2 + o 3 z 1 = 1 => 3
+5: o 1 z 4 = 3 + o 3 z 2 = 1 => 4
+6: o 1 z 5 = 4 + o 3 z 3 = 2 => 6
+*/
+
+const parse = input => {
+    let sections = input.split('\n\n');
+}
+
+const part1 = (input) => {
+}
+
+console.log('p1', part1(inputt));
