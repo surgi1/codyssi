a2f7a0bec46ddd5d437bec89546f9f73c38fa8d0
surgi <surgi1@gmail.com>
2025-05-12 20:41:21 +0200

    d18 p3

--- 2025/18/script.js
+++ 2025/18/script.js
@@ -1,22 +1,7 @@
 const parse = input => input.split('\n').map(line => line.match(/(\d|-)+/g).map(Number))
-
-const C = {
-    ID: 0,
-    X: 1,
-    Y: 2,
-    Z: 3,
-    A: 4,
-    DIV: 5,
-    REM: 6,
-    VX: 7,
-    VY: 8,
-    VZ: 9,
-    VA: 10,
-}
-
-const k = (x,y,z,a) => [x,y,z,a].join('_');
 const mod = (n, m) => ((n % m) + m) % m;
 const debrisPosInTime = (deb, t, dim = [10, 15, 60, 3]) => deb.p.map((pos, i) => mod(pos+deb.v[i]*t, dim[i]))
+const MOVES = [[1,0,0], [-1,0,0], [0,1,0], [0,-1,0], [0,0,1], [0,0,-1], [0,0,0]]; // submarine moves
 
 const initMap = (dim = [10, 15, 60], def = 0) => {
     let map = [];
@@ -24,7 +9,7 @@ const initMap = (dim = [10, 15, 60], def = 0) => {
         map[x] = [];
         for (let y = 0; y < dim[1]; y++) {
             map[x][y] = [];
-            for (let z = 0; z < dim[2]; z++) map[x][y][z] = def;
+            for (let z = 0; z < dim[2]; z++) map[x][y][z] = structuredClone(def);
         }
     }
     return map;
@@ -36,60 +21,48 @@ const mapInTime = (init, t, dim = [10, 15, 60]) => {
     let map = initMap(dim);
     init.forEach(deb => {
         let pos = debrisPosInTime(deb, t, dim);
-        if (pos[3] == 1) map[pos[0]][pos[1]][pos[2]] = map[pos[0]][pos[1]][pos[2]]-1;
+        if (pos[3] == 1) map[pos[0]][pos[1]][pos[2]]--;
     })
     map[0][0][0] = 0; // simulate no harm at 0,0,0
-    /*for (let x = 0; x < dim[0]; x++) for (let y = 0; y < dim[1]; y++) for (let z = 0; z < dim[2]; z++) {
-        if (map[x][y][z] == 0) map[x][y][z] = Number.POSITIVE_INFINITY;
-    }*/
     memo[t] = map;
     return map;
 }
 
 const part1 = (input, dim = [10, 15, 60, 3]) => {
     let rules = parse(input);
-    console.log(rules);
     let debris = [];
-    rules.forEach((r, i) => {
+    rules.forEach(([id, cx, cy, cz, ca, div, rem, vx, vy, vz, va], i) => {
         for (let x = 0; x < dim[0]; x++) for (let y = 0; y < dim[1]; y++) for (let z = 0; z < dim[2]; z++) for (let a = -1; a < -1 + dim[3]; a++) {
-            if (mod(r[C.X]*x + r[C.Y]*y + r[C.Z]*z + r[C.A]*a, r[C.DIV]) == r[C.REM]) debris.push({p: [x,y,z,a+1], v: [r[C.VX], r[C.VY], r[C.VZ], r[C.VA]]})
+            if (mod(cx*x + cy*y + cz*z + ca*a, div) == rem) debris.push({p: [x,y,z,a+1], v: [vx, vy, vz, va]})
         }
     })
-    console.log(debris);
     return debris;
 }
 
-const MOVES = [[1,0,0], [-1,0,0], [0,1,0], [0,-1,0], [0,0,1], [0,0,-1], [0,0,0]]; // submarine moves
-
-const dist = (a, b) => a.reduce((a, v, i) => a + Math.abs(v-b[i]), 0);
-
-// idea for p3 is to have each pt on the initial map [inf, inf, inf] - reflecting nr of lives left [3 lives, 2, 1] and alter the spread fnc to allow into those coords too
+// p2+p3 floodfill all the way
+// idea for p3 is to have each pt on the initial map [inf, inf, inf, inf] - reflecting nr of hits and alter the spread fnc to allow into those coords too
 // need to introduce separate map set for distances, can't be stored into the 1 set as for p2
 
-const part2 = (input, dim = [10, 15, 60, 3]) => {
+const part23 = (input, dim = [10, 15, 60, 3], maxHits = 1) => {
     let tMax = 220;
     let debris = part1(input, dim);
     let maps = [], dmaps = [];
     for (let t = 0; t <= tMax+1; t++) {
         maps[t] = mapInTime(debris, t, dim);
-        dmaps[t] = initMap(dim, Number.POSITIVE_INFINITY);
+        dmaps[t] = initMap(dim, [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY]);
     }
-    console.log('map by t', maps);
 
     let min = Number.POSITIVE_INFINITY;
 
-    const spread = (pos, t, d) => {
+    const spread = (pos, t, d, hits = 0) => {
         if (d > min) return;
         if (d > tMax) return;
-        if (maps[t][pos[0]][pos[1]][pos[2]] < 0) return;
-        if (dmaps[t][pos[0]][pos[1]][pos[2]] <= d) return;
-        dmaps[t][pos[0]][pos[1]][pos[2]] = d;
+
+        if (dmaps[t][pos[0]][pos[1]][pos[2]][hits] <= d) return;
+        dmaps[t][pos[0]][pos[1]][pos[2]][hits] = d;
 
         if (pos[0] == dim[0]-1 && pos[1] == dim[1]-1 && pos[2] == dim[2]-1) {
-            if (d < min) {
-                min = d;
-                console.log('****** new minimum path', min);
-            }
+            if (d < min) min = d;
             return;
         }
 
@@ -97,20 +70,18 @@ const part2 = (input, dim = [10, 15, 60, 3]) => {
         MOVES.forEach((move) => {
             let npos = [pos[0]+move[0], pos[1]+move[1], pos[2]+move[2]];
             if (npos[0] >= 0 && npos[1] >= 0 && npos[2] >= 0 &&
-                npos[0] < dim[0] && npos[1] < dim[1] && npos[2] < dim[2] && 
-                maps[t+1][npos[0]][npos[1]][npos[2]] > -1) spread(npos, t+1, d+1);
+                npos[0] < dim[0] && npos[1] < dim[1] && npos[2] < dim[2]) {
+                    let debriePcs = maps[t+1][npos[0]][npos[1]][npos[2]];
+                    if (hits - debriePcs < maxHits && hits - debriePcs >= 0) spread(npos, t+1, d+1, hits - debriePcs);
+                } 
         })
     }
 
-    spread([0,0,0], 0, 0)
+    spread([0,0,0], 0, 0, 0)
 
-    //console.log('memo', memo);
     return min;
 }
 
-//console.log('p1', part1(inputt, [3,3,5,3]));
-//console.log('p1', part1(input).length);
-
-//console.log('p2', part2(inputt, [3,3,5,3]));
-//console.log('p2', part2(inputt2));
-console.log('p2', part2(input));
+console.log('p1', part1(input).length);
+console.log('p2', part23(input));
+console.log('p3', part23(input, [10, 15, 60, 3], 4));
