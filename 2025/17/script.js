ecd00c6482c54125cf13e510218f79b73e4e5bd1
surgi <surgi1@gmail.com>
2025-05-16 18:29:36 +0200

    p3 for d17

--- 2025/17/script.js
+++ 2025/17/script.js
@@ -125,7 +125,108 @@ const part2 = (input) => {
     return paths2step[0][routes[0].endStep];
 }
 
-//console.log('p1', part1(input));
-console.log('p2', part2(input));
+// p3 idea: compute how many parts total lead from given step (on given path)
+// then the resulting path will be from those allowed steps that still have > 10^n ranked lower
+// do not forget to reduce the 10^n by taken steps as we progress
 
+const part3 = input => {
+    let [routes, steps] = parse(input);
+    let maxStep = Math.max(...steps); // 8
+
+    let nextStepsCache = {};
+    const target = 100000000000000000000000000000n;
+
+    const getNextSteps = (routeId, step) => {
+        let k = routeId+','+step;
+        if (nextStepsCache[k] !== undefined) return nextStepsCache[k];
+
+        let stack = [{routeId: routeId, step: step, len: 0}];
+        let res = {}, cur;
+
+        while (cur = stack.pop()) {
+            if (cur.len > maxStep) continue;
+            if (cur.step < routes[cur.routeId].startStep) continue;
+            if (cur.step > routes[cur.routeId].endStep) continue;
+            
+            // if we are exactly at the available step length, add it to the res
+            if (steps.includes(cur.len)) res[cur.routeId + ',' + cur.step] = 1;
+
+            // by default, continue up the current route
+            stack.push({routeId: cur.routeId, step: cur.step+1, len: cur.len+1});
+
+            // if we're at the start of any other route, go downstream
+            routes.filter(r => r.startRoute == cur.routeId && r.startStep == cur.step).forEach(r => stack.push({
+                routeId: r.id,
+                step: cur.step,
+                len: cur.len+1
+            }));
+
+            // if at the end of my route, go upstream
+            if (routes[cur.routeId].endStep == cur.step && !isNaN(routes[cur.routeId].endRoute)) stack.push({
+                routeId: routes[cur.routeId].endRoute,
+                step: cur.step,
+                len: cur.len+1
+            })
+        }
+
+        res = Object.keys(res);
+        res.sort((a, b) => {
+            let [ra, sa] = a.split(',').map(Number);
+            let [rb, sb] = b.split(',').map(Number);
+            if (ra != rb) return ra-rb;
+            return sa-sb;
+        })
+
+        nextStepsCache[k] = res;
+        return nextStepsCache[k];
+    }
+
+    const allPathsCache = {};
+
+    const getAllPathsFrom = (routeId, step) => {
+        let k = routeId+','+step;
+        if (allPathsCache[k] !== undefined) return allPathsCache[k];
+        if (step == routes[0].endStep) return 1n;
+        if (step >= routes[0].endStep) return 0n;
+        let nextSteps = getNextSteps(routeId, step);
+        let res = 0n;
+        res = nextSteps.reduce((a, v) => {
+            return a + getAllPathsFrom(...v.split(',').map(Number))
+        }, res);
+        allPathsCache[k] = res;
+        return allPathsCache[k];
+    }
+
+    const formatPath = cur => 'S'+(cur[0]+1)+'_'+(cur[1]);
+
+    console.log('p2', getAllPathsFrom(0,0)); // p2 result
+
+    // skim options from the first onwards and if we are within reach, pick that way. If we're not in the reach yet (allpaths[n] is lower than reached, deduce the reached and move on)
+    let cur = [0, 0], reached = target, path = [];
+
+    while (cur[0] != 0 || cur[1] != routes[cur[0]].endStep) {
+        path.push(formatPath(cur));
+        let nextSteps = getNextSteps(cur[0], cur[1]);
+        let pathPicked = false;
+        for (let i = 0; i < nextSteps.length; i++) {
+            let k = nextSteps[i].split(',').map(Number);
+            let v = getAllPathsFrom(...k);
+            if (v >= reached) {
+                cur = k;
+                pathPicked = true;
+                break;
+            }
+            reached -= v;
+        }
+    }
+
+    path.push(formatPath(cur));
+
+    return path.join('-');
+}
+
+//console.log('p1', part1(input));
+//console.log('p2', part2(input));
 //console.log('p2', part2(inputt2));
+
+console.log('p3', part3(input));
